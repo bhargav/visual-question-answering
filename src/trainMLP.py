e5d577a1e5beee9682787f4ea8bc755396eea833
Bhargav Mangipudi <bhargav.mangipudi@gmail.com>
2015-11-17 19:25:55 -0600

    Changes made

--- src/trainMLP.py
+++ src/trainMLP.py
@@ -5,6 +5,7 @@ from vqaTools.vqa import VQA
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout
+from keras.models import model_from_config
 
 import numpy as np
 import scipy.io as sio
@@ -44,6 +45,8 @@ def main():
     parser = argparse.ArgumentParser(description="MLP for ML-Project")
     parser.add_argument('--train_data_file')
     parser.add_argument('--train_label_file')
+    parser.add_argument('--model_weights_file')
+    parser.add_argument('--model_definition_file')
 
     args = parser.parse_args()
 
@@ -51,7 +54,16 @@ def main():
     label = np.load(open(args.train_label_file, 'r'))
 
     model = getMLPModel(data.shape[1], label.shape[1])
-    model.fit(X=data, y=label, verbose=True)
+
+    # Train for 10 iterations
+    model.fit(X=data, y=label, verbose=True, nb_epoch=10)
+
+    model.save_weights(args.model_weights_file, overwrite=True)
+
+    json_string = model.to_json()
+    if args.model_definition_file != None:
+        file = open(args.model_definition_file, 'w')
+        file.write(json_string)
 
 if __name__ == "__main__":
     main()
